a54103474f68e6a2735a5820ce8b5d5e6bfd4a55
Thony <thony.lituba@edouardgand.fr>
2026-02-05 16:47:37 +0100

    ajouter un fichier

--- views/one_pokemon.php
+++ views/one_pokemon.php
@@ -11,6 +11,18 @@
         <h2><?php echo $pokemon['name']; ?></h2>
         <p><strong>ID:</strong> <?php echo $pokemon['id']; ?></p>
         <p><strong>Type:</strong> <?php echo $pokemon['type']; ?></p>
+        <?php if (!empty($pokemon['evolutions'])): ?>
+            <h3>Évolutions</h3>
+            <div>
+                <?php foreach ($pokemon['evolutions'] as $evolution): ?>
+                    <div style="display: inline-block; margin: 10px; text-align: center;">
+                        <img src="<?php echo $evolution['image']; ?>" alt="<?php echo $evolution['name']; ?>" width="100">
+                        <p><?php echo $evolution['name']; ?></p>
+                        <a href="index.php?action=showOnePokemon&id=<?php echo $evolution['id']; ?>">Voir détail</a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
     </div>
     <a href="index.php?action=showAllPokemon">Retour à la liste</a>
 </body>
